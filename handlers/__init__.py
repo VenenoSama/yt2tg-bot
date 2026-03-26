2209125b1cacdefbf5c47ac57e470d1ea3a29e58
Daniel <danielsanchezrubio12@gmail.com>
2026-03-26 15:36:28 -0400

    # Expone todos los handlers del paquete para importar desde main.py
    Signed-off-by: Daniel <danielsanchezrubio12@gmail.com>

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,4 @@
-# Expone todos los handlers del paquete para importar desde main.py
 from .start import cmd_start
-from .download import handle_url_message, handle_format_selection
+from .download import handle_url_message, handle_format_selection, process_url, cmd_cancel
 
-__all__ = ["cmd_start", "handle_url_message", "handle_format_selection"]
-
-
-
+__all__ = ["cmd_start", "handle_url_message", "handle_format_selection", "process_url", "cmd_cancel"]
